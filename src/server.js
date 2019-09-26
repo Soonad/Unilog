c31ef0f2807719240f88dd87d8d8c439296cbdfd
Leonardo Souza <leonardostsouza@gmail.com>
2019-09-26 14:07:35 -0300

    feat: create a Hello World server

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const app = require('fastify')({logger: true});
+
+app.register(require('./routes/test-route'));
+
+module.exports = app.listen(3000, function (err, addr) {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+  app.log.info(`server listening on ${addr}`);
+});
