979ab967b5ddc8efeab7ed27d56ca94f21b12dcd
Leonardo Souza <leonardostsouza@gmail.com>
2019-09-30 19:11:48 -0300

    test: implement unit tests for API functions

--- test/test.js
+++ test/test.js
@@ -2,45 +2,156 @@ const chai = require('chai')
 const chaiHttp = require('chai-http')
 const app = require('../src/server')
 
-const url = ('http://localhost:3000')
+const url = ('http://localhost:3000/api')
 const should = chai.should()
+const expect = chai.expect
 chai.use(chaiHttp)
 
 describe('Unilog Server', () => {
-  describe('length: GET /streams/:stream_id', () => {
-    it('should return stream_id length', (done) => {
+
+  // =============== length(stream_id) Tests =============== //
+  describe('GET length: valid stream_id', () => {
+    it('should return stream_id length correctly', (done) => {
       chai.request(url)
         .get('/streams/0')
         .end((err, res) => {
           res.should.have.status(200)
-          res.body.should.be.a('object')
+          res.body.should.be.an('object')
+
+          const resp = JSON.parse(res.text)
+          expect(resp.length >= 0).to.be.true
+          resp.msg.should.be.empty
           done()
         })
     })
   })
 
-  describe('load: GET /streams/:stream_id/events', () => {
-    it('should return event list of stream_id', (done) => {
+  describe('GET length: invalid stream_id', () => {
+    it('should return null with error message', (done) => {
       chai.request(url)
-        .get('/streams/0/events?from=0&to=1')
+        .get('/streams/-1')
         .end((err, res) => {
           res.should.have.status(200)
-          res.body.should.be.a('object')
+          res.body.should.be.an('object')
+
+          const resp = JSON.parse(res.text)
+          expect(resp.length).to.be.null
+          resp.msg.should.not.be.empty
           done()
         })
     })
   })
 
-  describe('POST /streams/:stream_id/events', () => {
-    it('should return success', (done) => {
+  // =============== load(stream_id, from, to) Tests =============== //
+  describe('GET load: valid inputs', () => {
+    it('should return event list of stream_id', (done) => {
+      chai.request(url)
+        .get('/streams/0/events/0/1')
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.body.should.be.an('object')
+
+          const resp = JSON.parse(res.text)
+          expect(resp.events.length === 2).to.be.true
+          resp.events[0].should.be.an('object')
+          resp.events[1].should.be.an('object')
+          done()
+        })
+    })
+  })
+
+  describe('GET load: from == to', () => {
+    it('should return a single event', (done) => {
+      chai.request(url)
+        .get('/streams/0/events/0/0')
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.body.should.be.an('object')
+
+          const resp = JSON.parse(res.text)
+          expect(resp.events.length === 1).to.be.true
+          done()
+        })
+    })
+  })
+
+  describe('GET load: from > to', () => {
+    it('should return an empty array', (done) => {
+      chai.request(url)
+        .get('/streams/0/events/1/0')
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.body.should.be.an('object')
+
+          const resp = JSON.parse(res.text)
+          expect(resp.events).to.be.empty
+          done()
+        })
+    })
+  })
+
+  describe('GET load: from < 0', () => {
+    it('should return an empty array', (done) => {
+      chai.request(url)
+        .get('/streams/0/events/-1/0')
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.body.should.be.an('object')
+
+          const resp = JSON.parse(res.text)
+          expect(resp.events).to.be.empty
+          done()
+        })
+    })
+  })
+
+  describe('GET load: invalid stream_id', () => {
+    it('should return an empty array', (done) => {
+      chai.request(url)
+        .get('/streams/-1/events/0/1')
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.body.should.be.an('object')
+
+          const resp = JSON.parse(res.text)
+          expect(resp.events).to.be.empty
+          done()
+        })
+    })
+  })
+
+  // =============== push(stream_id, data) Tests =============== //
+  describe('POST push: valid stream_id', () => {
+    it('should return success code & message', (done) => {
       chai.request(url)
         .post('/streams/0/events')
         .send({
-          'data' : 'testData'
+          data: 'TestData'
         })
         .end((err, res) => {
           res.should.have.status(200)
           res.body.should.be.a('object')
+
+          const resp = JSON.parse(res.text)
+          expect(resp.success).to.be.true
+          done()
+        })
+    })
+  })
+
+  describe('POST push: invalid stream_id', () => {
+    it('should return push error code & message', (done) => {
+      chai.request(url)
+        .post('/streams/-1/events')
+        .send({
+          data: 'TestData'
+        })
+        .end((err, res) => {
+          res.should.have.status(201)
+          res.body.should.be.a('object')
+
+          const resp = JSON.parse(res.text)
+          expect(resp.success).to.be.false
           done()
         })
     })
