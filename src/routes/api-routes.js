f8043ec5875fcbace576f8b6facb633579b53b68
Leonardo Souza <leonardostsouza@gmail.com>
2019-10-02 11:03:30 -0300

    feat: update api methods and schemas

--- src/routes/api-routes.js
+++ src/routes/api-routes.js
@@ -2,6 +2,11 @@ const api_base_url = '/api';
 
 // Dummy data - For testing only
 // TODO: Use a proper database (Postgre) instead of hardcoded JSON
+
+// max size in bytes
+const stream_id_max_size = 16
+const data_max_size = 128
+
 var id0 = Buffer.from('0')
 var id1 = Buffer.from('1')
 
@@ -10,11 +15,9 @@ var data01 = Buffer.from('TEST_DATA: STREAM 0; EVENT 1')
 var data10 = Buffer.from('TEST_DATA: STREAM 1; EVENT 0')
 
 var dummy =
-[
-  {
-    name: 'stream1',
+{
+  MQ: {
     length: 2,
-    id: '0'/* id0.toString('base64') */,
     events:
       [
         { index: 0, data: data00.toString('base64') },
@@ -22,16 +25,14 @@ var dummy =
       ]
   },
 
-  {
-    name: 'stream2',
+  Mg: {
     length: 1,
-    id: '1'/* id1.toString('base64') */,
     events:
       [
         { index: 0, data: data10.toString('base64') }
       ]
   }
-]
+}
 
 // Data access functions
 // TODO: transfer these functions to a separate file
@@ -41,8 +42,8 @@ var dummy =
 // inputs: stream_id:Int
 // returns: Int
 async function length (stream_id) {
-  var len = null
-  if (stream_id < dummy.length && stream_id >= 0) {
+  var len = 0
+  if (dummy[stream_id] !== undefined) {
     len = dummy[stream_id].length
   }
   return len
@@ -54,7 +55,7 @@ async function length (stream_id) {
 // returns: [Object]
 async function load (stream_id, from, to) {
   var result = []
-  if (stream_id < dummy.length && stream_id >= 0) {
+  if (dummy[stream_id] !== undefined) {
     var events = dummy[stream_id].events
     if (from >= 0 && to >= from && from < events.length && to < events.length) {
       result = events.slice(from, to+1)
@@ -69,42 +70,73 @@ async function load (stream_id, from, to) {
 // returns: Object -- success/failure indicator
 async function push (stream_id, data) {
   var success = false
-  if (stream_id < dummy.length && stream_id >= 0) {
+  const base64_data = data.toString('base64')
+  if (dummy[stream_id] === undefined) {
+    dummy[stream_id] = {
+      length: 1,
+      events:
+        [{ index: 0, data: base64_data }]
+    }
+  } else {
     var len = dummy[stream_id].length
-    dummy[stream_id].events[len] = { index: len, data: data }
+    dummy[stream_id].events[len] = { index: len, data: base64_data }
     dummy[stream_id].length += 1
     success = true
   }
   return success
 }
 
+// Checks if base64 string is well formed
+function isBase64(base64_str) {
+    if (base64_str === '' || base64_str.trim() === ''){ return false; }
+    try {
+      const str_decode = Buffer.from(base64_str, 'base64').toString('ascii')
+      const str_reencode = Buffer.from(str_decode).toString('base64').replace(/=/g, '')
+      return str_reencode == base64_str
+    } catch (err) {
+        return false;
+    }
+}
+
+// checks if input size is valid
+function isSizeValid(base64_str, max_size) {
+  return Buffer.from(base64_str, 'base64').toString('ascii').length <= max_size
+}
+
 // =============== Route URLs =============== //
-const length_url = api_base_url + '/streams/:stream_id'
-const load_url = api_base_url + '/streams/:stream_id/events/:from/:to'
-const push_url = api_base_url + '/streams/:stream_id/events'
+const get_stream_url = api_base_url + '/streams/:stream_id'
+const get_stream_events_url = api_base_url + '/streams/:stream_id/events*'
+const push_event_url = api_base_url + '/streams/:stream_id/events'
 
 // =============== Route Schemas =============== //
-const length_schema =
+const get_stream_schema =
 {
   response: {
+    // Correct ID formatting
     200: {
       type: 'object',
       properties: {
-        stream: { type: 'string' },
+        stream_id: { type: 'string' },
         length: { type: 'integer' },
-        msg:  { type: 'string' },
       },
+    },
+    // Validation errors
+    400: {
+      type: 'object',
+      properties: {
+          error: { type: 'string' },
+      }
     }
   }
 }
 
-const load_schema =
+const get_stream_events_schema =
 {
   response: {
     200: {
       type: 'object',
       properties: {
-        stream: { type: 'string' },
+        stream_id: { type: 'string' },
         events: {
           type: 'array',
           items: {
@@ -117,102 +149,150 @@ const load_schema =
         },
       },
     },
+
+    400: {
+      type: 'object',
+      properties: {
+        error: {type: 'string'}
+      },
+    },
   },
 }
 
-const push_schema =
+const push_event_schema =
 {
   response: {
     200: {
       type: 'object',
-      properties: {
-        success: {type: 'boolean'},
-        msg: {type: 'string'},
-      },
     },
-    201: {
+    400: {
       type: 'object',
       properties: {
-        success: {type: 'boolean'},
-        msg: {type: 'string'},
+        error: {type: 'string'},
       },
     },
   },
 }
 
 // =============== Route Handlers =============== //
-async function length_handler (req, res) {
+async function get_stream_handler (req, res) {
   const stream_id = req.params.stream_id
-  const len = await length(stream_id)
-  const error_msg = (len === null) ? 'stream_id not found' : '';
+  var msg = ""
 
-  const resp =
-  {
-    stream: stream_id,
-    length: len,
-    msg : error_msg,
+  if (!isBase64(stream_id)) {
+    msg = "stream_id is malformed"
+  }
+  else if (!isSizeValid(stream_id, stream_id_max_size)){
+    msg = "stream_id is too big"
+  }
+  else {
+    const len = await length(stream_id)
+
+    const resp =
+    {
+      stream_id: stream_id,
+      length: len,
+    }
+
+    return JSON.stringify(resp)
   }
 
-  return JSON.stringify(resp)
+  res
+  .code(400)
+  .header('Content-Type', 'application/json; charset=utf-8')
+  .send({ error: msg })
 }
 
-async function load_handler (req, res) {
+async function get_stream_events_handler (req, res) {
   const stream_id = req.params.stream_id
-  const from = req.params.from
-  const to = req.params.to
+  const from = req.query.from
+  const to = req.query.to
 
-  const events_array = await load(stream_id, from, to)
+  if (!isBase64(stream_id)) {
+    msg = "stream_id is malformed"
+  }
+  else if (!isSizeValid(stream_id, stream_id_max_size)){
+    msg = "stream_id is too big"
+  }
+  else {
+    const events_array = await load(stream_id, from, to)
 
-  const resp = {
-    stream: stream_id,
-    events: events_array,
+    const resp = {
+      stream_id: stream_id,
+      events: events_array,
+    }
+
+    return JSON.stringify(resp)
   }
 
-  return JSON.stringify(resp)
+  res
+  .code(400)
+  .header('Content-Type', 'application/json; charset=utf-8')
+  .send({ error: msg })
 }
 
-async function push_handler (req, res) {
+async function push_event_handler (req, res) {
   const stream_id = req.params.stream_id
   const data = req.body.data
-  const is_successful = await push(stream_id, data)
-  var msg = "OK"
-  if (!is_successful) {
-    res.code(201)
-    msg = `An error occured when trying to push data to stream ${stream_id}`
+
+  var is_successful = false
+  var msg = ""
+
+  // verify inputs
+  var stream_id_size_valid = isSizeValid(stream_id, stream_id_max_size)
+  var data_size_valid = isSizeValid(data, data_max_size)
+
+  var stream_id_encoding_valid = isBase64(stream_id)
+  var data_encoding_valid = isBase64(data)
+
+  if (!stream_id_size_valid || !data_size_valid) {
+    msg = (data_size_valid ? "stream_id" : "data") + " is too long"
+  }
+  else if (!stream_id_encoding_valid || !data_encoding_valid) {
+    msg = (data_encoding_valid ? "stream_id" : "data") + " is malformed"
+  }
+  else {
+    is_successful = await push(stream_id, data)
+    msg = ""
   }
 
-  const resp = {
-    success: is_successful,
-    msg: msg
+  if (is_successful) {
+    res
+    .code(200)
+    .send({})
   }
-
-  return resp
+  else {
+    res
+    .code(400)
+    .header('Content-Type', 'application/json; charset=utf-8')
+    .send({ error: msg })
+  }
 }
 
 // =============== Routes =============== //
 const routes = [
-  // length
+  // get_stream
   {
     method: 'GET',
-    url: length_url,
-    schema: length_schema,
-    handler: length_handler
+    url: get_stream_url,
+    schema: get_stream_schema,
+    handler: get_stream_handler
   },
 
-  // load
+  // get_stream_events
   {
     method: 'GET',
-    url: load_url,
-    schema: load_schema,
-    handler: load_handler
+    url: get_stream_events_url,
+    schema: get_stream_events_schema,
+    handler: get_stream_events_handler
   },
 
-  // push
+  // push_event
   {
     method: 'POST',
-    url: push_url,
-    schema: push_schema,
-    handler: push_handler
+    url: push_event_url,
+    schema: push_event_schema,
+    handler: push_event_handler
   }
 ]
 
