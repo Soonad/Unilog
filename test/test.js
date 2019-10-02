ec88e13cb499a49e7c2d349feb3cdd949a83b588
Leonardo Souza <leonardostsouza@gmail.com>
2019-10-02 11:37:09 -0300

    test: update tests to match new api schemas and methods

--- test/test.js
+++ test/test.js
@@ -10,43 +10,58 @@ chai.use(chaiHttp)
 describe('Unilog Server', () => {
 
   // =============== length(stream_id) Tests =============== //
-  describe('GET length: valid stream_id', () => {
+  describe('GET get_stream: valid stream_id', () => {
     it('should return stream_id length correctly', (done) => {
       chai.request(url)
-        .get('/streams/0')
+        .get('/streams/MQ')
         .end((err, res) => {
           res.should.have.status(200)
           res.body.should.be.an('object')
 
           const resp = JSON.parse(res.text)
           expect(resp.length >= 0).to.be.true
-          resp.msg.should.be.empty
           done()
         })
     })
   })
 
-  describe('GET length: invalid stream_id', () => {
-    it('should return null with error message', (done) => {
+  describe('GET get_stream: malformed stream_id', () => {
+    it('should return stream_id malformed error message', (done) => {
       chai.request(url)
-        .get('/streams/-1')
+        .get('/streams/malformed_stream_id')
         .end((err, res) => {
-          res.should.have.status(200)
+          res.should.have.status(400)
           res.body.should.be.an('object')
 
           const resp = JSON.parse(res.text)
-          expect(resp.length).to.be.null
-          resp.msg.should.not.be.empty
+          resp.error.should.not.be.empty
+          expect(resp.error.includes("malformed")).to.be.true
+          done()
+        })
+    })
+  })
+
+  describe('GET get_stream: big stream_id', () => {
+    it('should return stream_id too long error message', (done) => {
+      chai.request(url)
+        .get('/streams/WW91Q2FtZVRvU2VlV2hhdEl0TWVhbnNSaWdodD9Zb3VBcmVBQ3VyaW91c0ZlbGxvdy4uLg')
+        .end((err, res) => {
+          res.should.have.status(400)
+          res.body.should.be.an('object')
+
+          const resp = JSON.parse(res.text)
+          resp.error.should.not.be.empty
+          expect(resp.error.includes("too")).to.be.true
           done()
         })
     })
   })
 
   // =============== load(stream_id, from, to) Tests =============== //
-  describe('GET load: valid inputs', () => {
+  describe('GET get_stream_events: valid inputs', () => {
     it('should return event list of stream_id', (done) => {
       chai.request(url)
-        .get('/streams/0/events/0/1')
+        .get('/streams/MQ/events?from=0&to=1')
         .end((err, res) => {
           res.should.have.status(200)
           res.body.should.be.an('object')
@@ -60,10 +75,10 @@ describe('Unilog Server', () => {
     })
   })
 
-  describe('GET load: from == to', () => {
+  describe('GET get_stream_events: from == to', () => {
     it('should return a single event', (done) => {
       chai.request(url)
-        .get('/streams/0/events/0/0')
+        .get('/streams/MQ/events?from=0&to=0')
         .end((err, res) => {
           res.should.have.status(200)
           res.body.should.be.an('object')
@@ -75,10 +90,10 @@ describe('Unilog Server', () => {
     })
   })
 
-  describe('GET load: from > to', () => {
+  describe('GET get_stream_events: from > to', () => {
     it('should return an empty array', (done) => {
       chai.request(url)
-        .get('/streams/0/events/1/0')
+        .get('/streams/MQ/events?from=1&to=0')
         .end((err, res) => {
           res.should.have.status(200)
           res.body.should.be.an('object')
@@ -90,10 +105,10 @@ describe('Unilog Server', () => {
     })
   })
 
-  describe('GET load: from < 0', () => {
+  describe('GET get_stream_events: from < 0', () => {
     it('should return an empty array', (done) => {
       chai.request(url)
-        .get('/streams/0/events/-1/0')
+        .get('/streams/MQ/events?from=-1&to=0')
         .end((err, res) => {
           res.should.have.status(200)
           res.body.should.be.an('object')
@@ -105,53 +120,87 @@ describe('Unilog Server', () => {
     })
   })
 
-  describe('GET load: invalid stream_id', () => {
-    it('should return an empty array', (done) => {
+  describe('GET get_stream_events: malformed stream_id', () => {
+    it('should return malformed message', (done) => {
       chai.request(url)
-        .get('/streams/-1/events/0/1')
+        .get('/streams/malformed_stream_id/events?from=0&to=1')
         .end((err, res) => {
-          res.should.have.status(200)
+          res.should.have.status(400)
           res.body.should.be.an('object')
 
           const resp = JSON.parse(res.text)
-          expect(resp.events).to.be.empty
+          resp.error.should.not.be.empty
+          expect(resp.error.includes("malformed")).to.be.true
+          done()
+        })
+    })
+  })
+
+  describe('GET get_stream_events: stream_id too long', () => {
+    it('should return an empty array', (done) => {
+      chai.request(url)
+        .get('/streams/WW91Q2FtZVRvU2VlV2hhdEl0TWVhbnNSaWdodD9Zb3VBcmVBQ3VyaW91c0ZlbGxvdy4uLg/events?from=0&to=1')
+        .end((err, res) => {
+          res.should.have.status(400)
+          res.body.should.be.an('object')
+
+          const resp = JSON.parse(res.text)
+          resp.error.should.not.be.empty
+          expect(resp.error.includes("too")).to.be.true
           done()
         })
     })
   })
 
   // =============== push(stream_id, data) Tests =============== //
-  describe('POST push: valid stream_id', () => {
-    it('should return success code & message', (done) => {
+  describe('POST push_event: valid stream_id', () => {
+    it('should return 200', (done) => {
       chai.request(url)
-        .post('/streams/0/events')
+        .post('/streams/MQ/events')
         .send({
-          data: 'TestData'
+          data: 'WW91SnVzdENhbnRTdGFuZEJlaW5nQ3VyaW91cw'
         })
         .end((err, res) => {
           res.should.have.status(200)
           res.body.should.be.a('object')
-
-          const resp = JSON.parse(res.text)
-          expect(resp.success).to.be.true
           done()
         })
     })
   })
 
-  describe('POST push: invalid stream_id', () => {
-    it('should return push error code & message', (done) => {
+  describe('POST push_event: malformed stream_id', () => {
+    it('should return stream_id malformed error message', (done) => {
       chai.request(url)
-        .post('/streams/-1/events')
+        .post('/streams/malformed_stream_id/events')
         .send({
-          data: 'TestData'
+          data: 'WW91SnVzdENhbnRTdGFuZEJlaW5nQ3VyaW91cw'
         })
         .end((err, res) => {
-          res.should.have.status(201)
+          res.should.have.status(400)
           res.body.should.be.a('object')
 
           const resp = JSON.parse(res.text)
-          expect(resp.success).to.be.false
+          resp.error.should.not.be.empty
+          expect(resp.error.includes("malformed")).to.be.true
+          done()
+        })
+    })
+  })
+
+  describe('POST push_event: big stream_id', () => {
+    it('should return stream_id too big error message', (done) => {
+      chai.request(url)
+        .post('/streams/WW91Q2FtZVRvU2VlV2hhdEl0TWVhbnNSaWdodD9Zb3VBcmVBQ3VyaW91c0ZlbGxvdy4uLg/events')
+        .send({
+          data: 'WW91SnVzdENhbnRTdGFuZEJlaW5nQ3VyaW91cw'
+        })
+        .end((err, res) => {
+          res.should.have.status(400)
+          res.body.should.be.a('object')
+
+          const resp = JSON.parse(res.text)
+          resp.error.should.not.be.empty
+          expect(resp.error.includes("too")).to.be.true
           done()
         })
     })
